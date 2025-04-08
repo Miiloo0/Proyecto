ba36ecde38682e2ecc312489b323e193d8fcc0e7
Miiloo0 <deapoolomg@gmail.com>
2025-04-07 21:17:02 -0500

    Update Conductor.java

--- Viaje/src/Empresa/Conductor.java
+++ Viaje/src/Empresa/Conductor.java
@@ -2,38 +2,62 @@ package Empresa;
 
 public class Conductor extends Personas {
 
-	@Override
-	public int getidentificacion(int id) {
-		return id;
+	private long numDocumento;
+
+	private String nombre;
+
+	private String Apellido;
+	
+	private boolean Licencia;
+
+	public Conductor() { 
+	
+		super();
+	}
+
+	public Conductor (long numDocumento, String nombre, String apellido, boolean Licencia) {
+		super();
+		this.numDocumento = numDocumento;
+		this.nombre = nombre;
+		this.Apellido = apellido;
+		this.Licencia = Licencia;
+	}
+	
+	public boolean getLicenciaVal() {
+		return Licencia;
+	}
+	
+	public void setLicenciaVal(boolean Licencia) {
+	
+	}
+
+	public long getNumDocumento() {
+		return numDocumento;
+	}
+
+	public void setNumDocumento(long numDocumento) {
+		this.numDocumento = numDocumento;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.Apellido = apellido;
 	}
 
 	@Override
-	public String getNombre(String Name) {
-		return Name;
+	public String toString() {
+		return ", numDocumento=" + numDocumento + ", nombre=" + nombre + ", apellido=" + Apellido + "]";
 	}
 
-	@Override
-	public void setCantidadDeID(int cantidad) {
-	}
-
-	@Override
-	public int getCantidadDeID() {
-		return 0;
-	}
-
-	public Boolean verificarLicencia() {
-		int id = getCantidadDeID();
-		Boolean licencia = false;
-
-		if (id == getIdentificacion(id)) {
-			return true;
-		}
-
-		return licencia;
-	}
-
-	public Boolean AsignacionDeRutaS() {
-
-		return null;
-	}
 }
