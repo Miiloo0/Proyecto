2d42b52f3acb26d363e14ca5074f764c3b57b72b
Miiloo0 <deapoolomg@gmail.com>
2025-04-08 07:38:57 -0500

    Update Pasajero.java

--- Viaje/src/Empresa/Pasajero.java
+++ Viaje/src/Empresa/Pasajero.java
@@ -1,5 +1,55 @@
 package Empresa;
 
-public class Pasajero {
+import java.util.List;
+
+public class Personas {
+
+	private long numDocumento;
+
+	private String nombre;
+
+	private String Apellido;
+
+  private int Boletos
+
+	public Personas() {
+		super();
+	}
+
+	public Personas(long numDocumento, String nombre, String apellido) {
+		super();
+		this.numDocumento = numDocumento;
+		this.nombre = nombre;
+		this.Apellido = Apellido;
+	}
+
+	public long getNumDocumento() {
+		return numDocumento;
+	}
+
+	public void setNumDocumento(long numDocumento) {
+		this.numDocumento = numDocumento;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.Apellido = apellido;
+	}
+
+	@Override
+	public String toString() {
+		return ", numDocumento=" + numDocumento + ", nombre=" + nombre + ", apellido=" + Apellido + "]";
+	}
 
 }
